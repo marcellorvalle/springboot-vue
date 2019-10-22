5841d27141db6ffe1a8fbd929eb18679e285ec28
Marcello <marcello.valle@ufjf.edu.br>
2019-10-22 19:11:01 -0200

    Configurações cors

--- backend/src/main/java/com/tutospringvue/backend/configuration/WebConfig.java
+++ backend/src/main/java/com/tutospringvue/backend/configuration/WebConfig.java
@@ -1,15 +1,41 @@
 package com.tutospringvue.backend.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${app.origins}")
+    private String[] origins;
+    @Value("${app.methods}")
+    private String[] methods;
+    @Value("${app.headers}")
+    private String[] headers;
+    @Value("${app.access-control-age}")
+    private Long maxAge;
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins(origins)
+                .allowedHeaders(headers)
+                .allowedMethods(methods)
+                .maxAge(maxAge);
     }
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("forward:/index.html");
+//    }
+//
+//        @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**")
+//                .addResourceLocations("classpath:/static/");
+//    }
 }
--- backend/src/main/java/com/tutospringvue/backend/controller/HelloController.java
+++ backend/src/main/java/com/tutospringvue/backend/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.tutospringvue.backend.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/hello")
     public String hello() {
+        logger.info("Requisição recebida!");
         return "Hello world!";
     }
 }
