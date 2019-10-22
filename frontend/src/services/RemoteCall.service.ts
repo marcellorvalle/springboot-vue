42fc9919c888f9d34b3bc49ac8ad270928c16085
Marcello <marcello.valle@ufjf.edu.br>
2019-10-22 19:45:43 -0200

    Comunicação front-back

--- frontend/src/services/RemoteCall.service.ts
+++ frontend/src/services/RemoteCall.service.ts
@@ -1,9 +1,10 @@
-import Axios, { AxiosResponse } from 'axios';
+import Axios, {AxiosResponse} from 'axios';
 
 class RemoteCall {
-  public doCall(): Promise<AxiosResponse> {
-    return Axios.get('http://localhost:8080/hello');
-  }
+    public doCall(): Promise<AxiosResponse> {
+        const root = process.env.VUE_APP_ROOT_API ? process.env.VUE_APP_ROOT_API : window.location.origin;
+        return Axios.get(root + '/hello');
+    }
 }
 
 export default new RemoteCall();
